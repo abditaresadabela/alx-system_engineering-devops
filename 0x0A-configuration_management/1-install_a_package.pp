5ee74c9c6c068c064707ee6a5746d965cd5f5f14
abditaresadabela <oumuamua192017@gmail.com>
2023-03-29 23:09:15 +0530

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,5 @@
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
-}
-
-file { '/usr/bin/flask':
-  ensure => link,
-  target => '/usr/local/bin/flask',
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
