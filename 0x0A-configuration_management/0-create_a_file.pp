798aed9f59a77cfc61023a52460d30a3879dee56
abditaresadabela <oumuamua192017@gmail.com>
2023-03-29 22:48:32 +0530

    configuration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# This Puppet manifest creates a file in /tmp
-#
-# file { 'school':
-# ensure  => file,
-# path    => '/tmp/school',
-# mode    => '0744',
-# owner   => 'www-data',
-# group   => 'www-data',
-# content => 'I love Puppet'
-# } 
+# creates a file in /tmp
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
