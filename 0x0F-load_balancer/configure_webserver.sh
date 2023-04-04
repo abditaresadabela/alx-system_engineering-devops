849e36d7a3c6c6ae821d76953e4c8a1f303a4dcc
abditaresadabela <oumuamua192017@gmail.com>
2023-04-04 20:32:55 +0530

    load

--- 0x0F-load_balancer/configure_webserver.sh
+++ 0x0F-load_balancer/configure_webserver.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get -y install nginx
 
 # Set up custom HTTP response header
-sudo sed -i 's/^# server_tokens off;/server_tokens off;\n        add_header X-Served-By $121750-web-02;\n/' /etc/nginx/nginx.conf
+sudo sed -i 's/^# server_tokens off;/server_tokens off;\n        add_header X-Served-By $121750-web-01;\n/' /etc/nginx/nginx.conf
 
 # Restart Nginx
 sudo service nginx restart
