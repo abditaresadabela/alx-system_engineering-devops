adc3b235cc541cc79da3bae9251dfcb5c434089f
abditaresadabela <oumuamua192017@gmail.com>
2024-08-15 15:01:53 +0530

    API

---
+++ 0x16-api_advanced/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+from 0-subs import number_of_subscribers
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
